8f8fafeff2c158d4b2320e8013143e6eb7b891cf
nick ackerman <nckackerman@gmail.com>
2018-04-27 22:45:39 -0700

    stopping start.sh so it doesnt time out during codeDeploy

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 # Ensure all java instances are dead
 killall -9 java
 
-java -jar /var/www/streamercontracts/build/libs/springhelloworld-1.0-SNAPSHOT.jar
+java -jar /var/www/streamercontracts/build/libs/springhelloworld-1.0-SNAPSHOT.jar & exit $?
