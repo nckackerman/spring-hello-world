b868b3b0e705020a8316bd058609dbd59ef750d4
nick ackerman <nckackerman@gmail.com>
2018-04-28 19:39:52 -0700

    removing incorrect code-pipeline.json

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "StreamerContracts test Endpoint. Test for continuous deployment";
+        return "StreamerContracts test Endpoint. Test for continuous deployment - v2";
     }
 
 }
