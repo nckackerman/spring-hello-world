9e002fa9672d68b7f0317d45e5c3ef9646c90560
nick ackerman <nckackerman@gmail.com>
2018-05-07 21:28:41 -0700

    fixing start.sh and build_spec.yml

--- infrastructure/start.sh
+++ infrastructure/start.sh
@@ -8,12 +8,12 @@ echo "group name: '${DEPLOYMENT_GROUP_NAME}'"
 if [[ ${DEPLOYMENT_GROUP_NAME} =~ "Web" ]]
 then
     echo "Starting StreamerContracts-Web SpringBoot Application"
-    java -jar /var/www/streamercontracts/web/build/libs/web-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
+    java -jar /var/www/streamercontracts/web-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
     exit $?
 elif [[ ${DEPLOYMENT_GROUP_NAME} =~ "Api" ]]
 then
     echo "Starting StreamerContracts-Api SpringBoot Application"
-    java -jar /var/www/streamercontracts/api/build/libs/api-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
+    java -jar /var/www/streamercontracts/api-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
     exit $?
 else
     echo "Invalid deployment group name '${DEPLOYMENT_GROUP_NAME}', no role found " 2>&1 &
