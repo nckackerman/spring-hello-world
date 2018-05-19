7ba85baff5ed2312d580c584607de1ca45136e4e
nick ackerman <nckackerman@gmail.com>
2018-05-18 22:48:16 -0700

    Adding Nginx back webserver ec2 instance. Includes nginx.conf file to handle routing from the webserver to either api host or web host based on http route.

--- infrastructure/start.sh
+++ infrastructure/start.sh
@@ -3,9 +3,20 @@
 # Ensure all java instances are dead
 killall -9 java
 
-echo "group name: '${DEPLOYMENT_GROUP_NAME}'"
-
-if [[ ${DEPLOYMENT_GROUP_NAME} =~ "Web" ]]
+if [[ ${DEPLOYMENT_GROUP_NAME} =~ "WebServer" ]]
+then
+    echo "Starting nginx StreamerContracts-WebServer"
+    # This is hack to make nginx work with environment variables.
+    # Currently points traffic directly to ec2 instances. We'll likely want this to point to autoscaling groups in the future
+    # make sure $WEB_HOST, $WEB_PORT, $API_HOST, and $API_PORT get set while spinning up StreamerContracts-WebServer instances
+    sudo cp /var/www/streamercontracts/nginx.conf /etc/nginx/nginx.conf
+    sudo sed -i "s|REPLACE_WITH_WEB_HOST|$WEB_HOST|g" /etc/nginx/nginx.conf
+    sudo sed -i "s|REPLACE_WITH_WEB_PORT|$WEB_PORT|g" /etc/nginx/nginx.conf
+    sudo sed -i "s|REPLACE_WITH_API_HOST|$API_HOST|g" /etc/nginx/nginx.conf
+    sudo sed -i "s|REPLACE_WITH_API_PORT|$API_PORT|g" /etc/nginx/nginx.conf
+    sudo nginx &
+    exit $?
+elif [[ ${DEPLOYMENT_GROUP_NAME} =~ "Web" ]]
 then
     echo "Starting StreamerContracts-Web SpringBoot Application"
     java -jar /var/www/streamercontracts/web-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
--- web/src/main/resources/static/js/hello.js
+++ web/src/main/resources/static/js/hello.js
@@ -1,6 +1,18 @@
 class Greeting extends React.Component {
+
+    handleClick() {
+        axios.get('api').then(response => {
+            alert('/api response: ' + response.data);
+        })
+    }
+
     render() {
-        return (<p>Hello From React!</p>);
+        return (
+            <div>
+                <p>Hello From React!</p>
+                <a onClick={this.handleClick}>Click me to make a request to /api... if you get an alert, this is working</a>
+            </div>
+        );
     }
 }
 ReactDOM.render(
